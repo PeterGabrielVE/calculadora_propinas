f3ff88ebdbfd95f39fda7d1d325a8c6f8c9e7716
Pedro Gabriel <gaboleal123@gmail.com>
2024-07-13 09:20:00 -0400

    creando header

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-orange-400">holaaa</h1>
-      <h1 className="text-orange-400">holaaa2</h1>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-block">Calculadora de Propinas y Consumo</h1>
+      </header>
     </>
   )
 }
