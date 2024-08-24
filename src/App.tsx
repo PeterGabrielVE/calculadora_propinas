8da35846080a5278bb5959f5b55d0e6bf84be696
Pedro Gabriel <gaboleal123@gmail.com>
2024-08-23 21:56:18 -0400

    listando los productos

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,25 @@ function App() {
   return (
     <>
       <header className="bg-teal-400 py-5">
-        <h1 className="text-center text-4xl font-block">Calculadora de Propinas y Consumo</h1>
+        <h1 className="text-center text-4xl font-block">
+          Calculadora de Propinas y Consumo
+        </h1>
       </header>
       <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2">
-          <div>
-            <h2>Menú</h2>
-
-            {menuItems.map(item =>(
-              <MenuItem key={
-                item.id
-              } />
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} />
             ))}
           </div>
-          <div>
-            <h2>Consumo</h2>
-          </div>
+        </div>
+        <div>
+          <h2>Consumo</h2>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,17 @@
+import type { menuItem } from "../types"
 
-export default function MenuItem() {
+type MenuItemProps = {
+  item: menuItem
+}
+
+export default function MenuItem({item} : MenuItemProps) {
   return (
-    <div>MenuItem</div>
+    <button
+      className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between mb-5"
+    >
+      <p>{item.name}</p>
+      <p className="font-black">${item.price}</p>
+    </button>
   )
 }
 
