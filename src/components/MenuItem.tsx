9c99e930239feab851f0b04d6dce3b14481dbb72
Pedro Gabriel <gaboleal123@gmail.com>
2024-09-24 20:46:08 -0400

    creando hook

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,13 +1,15 @@
 import type { menuItem } from "../types"
 
 type MenuItemProps = {
-  item: menuItem
+  item: menuItem,
+  addItem: () => void
 }
 
-export default function MenuItem({item} : MenuItemProps) {
+export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
     <button
       className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between mb-5"
+      onClick={ () => addItem() }
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
