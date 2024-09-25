9fc18939e8f87948a8307f7ea41fcb9f8b03797b
Pedro Gabriel <gaboleal123@gmail.com>
2024-09-24 20:52:00 -0400

    asignando el signature

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,15 +1,15 @@
-import type { menuItem } from "../types"
+import { menuItem } from "../types"
 
 type MenuItemProps = {
   item: menuItem,
-  addItem: () => void
+  addItem: (item: menuItem) => void
 }
 
 export default function MenuItem({item, addItem} : MenuItemProps) {
   return (
     <button
       className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between mb-5"
-      onClick={ () => addItem() }
+      onClick={ () => addItem(item) }
     >
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,13 +1,14 @@
 import { useState } from "react";
-import type { OrderItem } from '../types';
+import type { menuItem, OrderItem } from '../types';
+
 
 export default function useOrder() {
     const [order, setOrder] = useState<OrderItem[]>([]);
     //const [total, setTotal] = useState(0);
    // const [auth, setAuth] = useState(false);
 
-    const addItem = () =>{
-        console.log('agregando....')
+    const addItem = (item: menuItem) =>{
+        console.log(item)
     }
     return {
         addItem
