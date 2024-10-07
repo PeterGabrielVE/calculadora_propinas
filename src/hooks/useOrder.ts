2a6318ebd821d3f8be91f43572a99818372d3cde
Pedro Gabriel <gaboleal123@gmail.com>
2024-10-07 08:14:19 -0400

    evitando duplicado

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,16 +1,25 @@
 import { useState } from "react";
-import type { menuItem, OrderItem } from '../types';
-
+import type { menuItem, OrderItem } from "../types";
 
 export default function useOrder() {
-    const [order, setOrder] = useState<OrderItem[]>([]);
-    //const [total, setTotal] = useState(0);
-   // const [auth, setAuth] = useState(false);
+  const [order, setOrder] = useState<OrderItem[]>([]);
 
-    const addItem = (item: menuItem) =>{
-        console.log(item)
+  const addItem = (item: menuItem) => {
+   
+    const itemExist = order.find(orderItem => orderItem.id === item.id);
+
+    if(itemExist){
+       const updatedOrder = order.map(orderItem => orderItem.id === item.id ? {
+            ...orderItem, quantity: orderItem.quantity + 1
+        } : orderItem);
+        setOrder(updatedOrder);
+    }else{
+        const newItem: OrderItem = { ...item, quantity: 1 };
+        setOrder([...order, newItem]);
     }
-    return {
-        addItem
-    }
+    console.log(order)
+  };
+  return {
+    addItem,
+  };
 }
